b8009d3f4f97aa3deacc364611d405360df5db96
tingtsz124 <tingtsz124@gmail.com>
2019-06-15 19:10:38 +0800

    add index.html

--- playlist/playList.js
+++ playlist/playList.js
@@ -1,11 +1,11 @@
 var playList=[
-    "L229QDxDakU",
-    "kfXdP7nZIiE",
-    "x3bDhtuC5yk"
+    "iIPH8LFYFRk",
+    "2GRP1rkE4O0",
+    "btDd9rOlc2k"
 ];
 
 var playTime=[
-    [73,80],
-    [1,5],
-    [116,120]
+    [66,76],
+    [14,27],
+    [180,196]
 ];
